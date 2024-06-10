c858019b7d87917c994e1be4afce23fb5bd440e0
ngoc <ngocdb1609@gmail.com>
2024-06-10 23:04:59 +0900

    minor syntax err

--- commands/config.js
+++ commands/config.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const Guild = require('../models/Guild');
 
 module.exports = {
